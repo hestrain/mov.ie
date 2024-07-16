c463d6d47089cdbdf7927b20cdb9ba3b751a0cdf
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-07-16 11:14:28 -0400

    search function on this page works adn brings you back to the search page

--- assets/js/mov.js
+++ assets/js/mov.js
@@ -1,5 +1,4 @@
-// for testing im setting hard movie and movie id, but eventually these will be from a user input
-
+//variables of needed document elements
 const movieZone = document.querySelector("#movie-info");
 const streamingZone = document.querySelector("#streaming-info");
 const titleEl = document.querySelector("#title");
@@ -9,6 +8,18 @@ const posterEl = document.querySelector("#poster");
 const directorEl = document.querySelector("#director");
 const castEl = document.querySelector("#cast");
 
+//the input element of the searchbar so we can get data from it
+const searchLine = document.querySelector("#tiny-input");
+console.log(searchLine);
+
+//the button element of teh searchbar for the event listener
+const searchButton = document.querySelector("#search-btn");
+
+//get movie from storage....
+let movie = JSON.parse(localStorage.getItem("movie"));
+
+
+// for testing im setting hard movie and movie id, but eventually these will be from a user input
 //for testing purposes i'm setting this. in reality it'll be from user input
 const testMovie = {
   id: "tt2283362",
@@ -18,6 +29,7 @@ const testMovie = {
     "https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
 };
 
+//setting ott key as a variable so its easier to change when we max out fetches
 const ottKey = "7e934c17e2mshcd85f90de49e405p139dacjsn81feb4b9bbf3"; //eliots key
 
 //setting localstorage
@@ -35,17 +47,17 @@ function getOTT() {
       "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
     },
   };
-
+//fetch the data
   fetch(ottUrl, ottOptions)
     .then(function (response) {
+      //return data as json
       return response.json();
     })
     .then(function (services) {
       console.log(" STREAM INFO \n----------");
+      //log the whole thing so i can figure out what data we need
       console.log(services);
-      // get info from response
-      console.log(services.directors);
-      console.log(services.releaseYear);
+      // then get info from response
 
       //set description, direcor, cast
       descEl.textContent = `Description: ${services.overview}`;
@@ -53,7 +65,7 @@ function getOTT() {
       //print out the content of directors
       directorEl.textContent = `Director: ${services.directors[0]}`;
 
-      //print each cast member
+      //print each cast member as a list item
       for (let i = 0; i < services.cast.length; i++) {
         const actor = services.cast[i];
        //create list element with actor name
@@ -94,3 +106,23 @@ function renderPage() {
 
 //call render page which calls the OTT streaming info function to fill out the page
 renderPage();
+
+//function to deal with search query
+function searchHandler(event) {
+  //prevent refresh
+  event.preventDefault();
+  //log to check
+  console.log(searchLine.value.trim());
+  //set movie to what user searchd
+  movie = searchLine.value.trim();
+
+  //im storing it to localstorage just in case we need it in this format?
+  localStorage.setItem("movie", JSON.stringify(movie));
+
+  //reser the searchbar
+  $(searchLine).text = "";
+  //take you to the search page NEEDS TO BE LAST THING IN FUNCTION
+  window.location.href = "./search.html";
+}
+
+searchButton.addEventListener("click", searchHandler);
